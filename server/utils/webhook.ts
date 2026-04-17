d1af51f8a50a208ed271fa362da91e43a0bb6903
Bank987 <beyongodperk@gmail.com>
2026-04-17 08:43:04 +0700

    UPDATE WEBHOOK.TS

--- server/utils/webhook.ts
+++ server/utils/webhook.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export const sendWebhook = async (type: 'admin' | 'public', payload: any) => {
-  const webhookUrl = type === 'admin' ? process.env.WEBHOOK_ADMIN_URL : process.env.WEBHOOK_PUBLIC_URL;
+export type WebhookType = 'admin' | 'public' | 'gacha' | 'upgrade';
+
+export const sendWebhook = async (type: WebhookType, payload: any) => {
+  let webhookUrl = process.env.WEBHOOK_PUBLIC_URL;
+  
+  if (type === 'admin') webhookUrl = process.env.WEBHOOK_ADMIN_URL;
+  if (type === 'gacha') webhookUrl = process.env.WEBHOOK_GACHA_URL;
+  if (type === 'upgrade') webhookUrl = process.env.WEBHOOK_UPGRADE_URL;
 
   if (!webhookUrl) return;
 
